99b179755cbd9a8c83cc395dbd23e908af5eba46
Michael <michael.tubul@gmail.com>
2025-10-31 23:13:52 +0200

    Initial Commit

---
+++ thinqsync/thinqsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  thinqsyncApp.swift
+//  thinqsync
+//
+//  Created by Michael   on 31/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct thinqsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ thinqsyncTests/thinqsyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  thinqsyncTests.swift
+//  thinqsyncTests
+//
+//  Created by Michael   on 31/10/2025.
+//
+
+import Testing
+@testable import thinqsync
+
+struct thinqsyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
